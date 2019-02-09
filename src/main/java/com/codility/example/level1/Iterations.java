ed66fa7f0fb1a50f7df0728ec4844a1954c7fbae
choonghyun <choonghyun1.yang@cj.net>
2019-02-09 13:25:04 +0900

    codility level test1

---
+++ src/main/java/com/codility/example/level1/Iterations.java
@@ -0,0 +1,13 @@
+package com.codility.example.level1;
+
+/**
+ * Created by choonghyun on 09/02/2019.
+ */
+public class Iterations {
+
+    public int solutions(int n) {
+
+        return Integer.valueOf(Integer.toBinaryString(n));
+
+    }
+}
