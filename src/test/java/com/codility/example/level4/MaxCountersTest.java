0107af5b63d0fe9db36d6f332c85134e6aacf994
choonghyun <choonghyun1.yang@cj.net>
2019-02-20 23:40:06 +0900

    100%를 달성하기위해서 삽질중... 줄이고 줄이는데 안되넹.

--- src/test/java/com/codility/example/level4/MaxCountersTest.java
+++ src/test/java/com/codility/example/level4/MaxCountersTest.java
@@ -5,8 +5,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Arrays;
-
 /**
  * Created by choonghyun on 19/02/2019.
  */
@@ -19,23 +17,31 @@ public class MaxCountersTest {
     public void solution1() {
         int N = 5;
         int A[] = {3, 4, 4, 6, 1, 4, 4};
-        int[] result = maxCounters.solution1(N, A);
+        int[] result = maxCounters.solution2(N, A);
         for (int i : result) {
             log.info("result {} ", i);
         }
     }
 
     @Test
-    public void initArray() {
-        int A[] = new int[100001];
-        Arrays.fill(A, 10);
-        log.info("start");
-        for (int i : A) {
-            i++;
-
+    public void solution2() {
+        int N = 5;
+        int A[] = {6, 6, 6, 6, 6, 6};
+        int[] result = maxCounters.solution2(N, A);
+        for (int i : result) {
+            log.info("result {} ", i);
         }
 
-        log.info("end");
+    }
+
+    @Test
+    public void solution3() {
+        int N = 2;
+        int A[] = {2};
+        int[] result = maxCounters.solution2(N, A);
+        for (int i : result) {
+            log.info("result {} ", i);
+        }
 
     }
 }
